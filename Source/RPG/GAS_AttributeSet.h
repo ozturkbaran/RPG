7acd1a7931223ff66e9b914969c8781e0a522622
Baranoztk <ozturk.baran@hotmail.com>
2024-03-25 18:44:53 +0300

    Some of c++ classes added
    Mygameabilityability
    GAS_AttributeSet
    GAS_GameplayComponent

---
+++ Source/RPG/GAS_AttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "GAS_AttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API UGAS_AttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
